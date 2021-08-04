a2ba444010ac16036fbc7f1f3c96907bce4eb1a8
Osh <gusmunos@student.gu.se>
2021-08-04 15:38:41 +0200

    Revert "utility about duation yoyo ect."
    This reverts commit e0d84bfb969e3bca9e4cdb461c18362f3312f99e.

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
 import { gsap } from "gsap";
 
-
+//Easing, go Gsap Doc -> Easing, it's very fun!
 const img1 = document.querySelector('.img1')
 gsap.from(img1, {
   autoAlpha: 0,
    y:-100,
   rotation: 90,
   ease: "elastic.out(1,0.3)",
-  duration: 2,
-    repeat: -1,
-    yoyo: true,
-    repeatDelay: 0.9
+  duration: 2
 })
